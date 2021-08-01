efd16ae7c642f227c02e997c24034e1ecc241f45
Shailesh Aanand <anaandshailu@gmail.com>
2021-08-01 20:06:04 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  purge: ["*.html", "**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
